4828f50bbe783916c7131c598f0b2f690a39f0ac
a4594865-crypto <a4594865@gmail.com>
2026-02-27 06:53:51 +0800

    Update Main.cs

--- src/Main.cs
+++ src/Main.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SwiftlyS2.Shared;
+using SwiftlyS2.Shared.Player;
 using SwiftlyS2.Shared.Commands;
 using SwiftlyS2.Shared.Events;
 using SwiftlyS2.Shared.GameEventDefinitions;
