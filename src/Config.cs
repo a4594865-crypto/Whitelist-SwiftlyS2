c24a25b672c083f8973aaab2d950897b0d0467f2
a4594865-crypto <a4594865@gmail.com>
2026-02-27 06:36:04 +0800

    Update Config.cs

--- src/Config.cs
+++ src/Config.cs
@@ -6,16 +6,11 @@ namespace Whitelist;
 
 public class PluginConfig
 {
-    // 模式設定：1 為白名單，2 為黑名單
     public int Mode { get; set; } = 1;
-
-    // 指令名稱
     public string AddCommand { get; set; } = "wladd";
     public string RemoveCommand { get; set; } = "wlremove";
-
-    // 權限設定
     public string PermissionForCommands { get; set; } = "@css/root";
     
-    // 新增這行，解決 'PluginConfig' 不包含 'AdminExemptPermission' 的錯誤
+    // 確保這一行存在，主程式才能編譯
     public string AdminExemptPermission { get; set; } = "@css/root"; 
 }
