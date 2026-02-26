a4482cda12ad1c1503c9be7bd9acbf9d4afc9f01
a4594865-crypto <a4594865@gmail.com>
2026-02-26 15:56:37 +0800

    Add Admins property to PluginConfig
    Add Admins property to PluginConfig for managing admin list.

--- src/Config.cs
+++ src/Config.cs
@@ -1,11 +1,12 @@
-namespace Whitelist;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace Whitelist;
 public class PluginConfig
 {
     public int Mode { get; set; } = 1;
     public string AddCommand { get; set; } = "wl";
     public string RemoveCommand { get; set; } = "uwl";
     public string PermissionForCommands { get; set; } = "admin.ban";
-    // 增加這一行，讓程式可以讀取管理員清單
-    public List<string> Admins { get; set; } = new(); 
 }
