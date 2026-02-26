d6b0aae96ed8c13f000431e55af5214c782a8ac0
a4594865-crypto <a4594865@gmail.com>
2026-02-27 06:32:52 +0800

    Update Config.cs

--- src/Config.cs
+++ src/Config.cs
@@ -1,12 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Whitelist;
+
 public class PluginConfig
 {
+    // 模式設定：1 為白名單，2 為黑名單
     public int Mode { get; set; } = 1;
-    public string AddCommand { get; set; } = "wl";
-    public string RemoveCommand { get; set; } = "uwl";
-    public string PermissionForCommands { get; set; } = "admin.ban";
+
+    // 指令名稱
+    public string AddCommand { get; set; } = "wladd";
+    public string RemoveCommand { get; set; } = "wlremove";
+
+    // 權限設定
+    public string PermissionForCommands { get; set; } = "@css/root";
+    
+    // 新增這行，解決 'PluginConfig' 不包含 'AdminExemptPermission' 的錯誤
+    public string AdminExemptPermission { get; set; } = "@css/root"; 
 }
